22340694ce60ed4a17332e0f22ab8061f207ba81
Miguel Rass <miguel_rass@hotmail.com>
2016-05-15 23:33:52 +0100

    simplificado e a funceminar

--- vic_irq.v
+++ vic_irq.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
+// Company: Rass Industries
 // Engineer: 
 // 
 // Create Date: 05/13/2016 04:00:23 PM
 // Design Name: 
 // Module Name: vic_irq
-// Project Name: 
+// Project Name: VICtor
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -28,7 +28,6 @@ module vic_irq(
     input i_rst,
     input [30:0] i_ext,
     input i_en,
-    //output o_pending, //outra interrupcao alem da atual
     output reg [4:0] o_irq_addr //num. interr atual
     );
     
@@ -57,32 +56,29 @@ end
 
 integer j;
 
-always@(negedge i_IRQ) begin
-    if(i_reg[4*o_irq_addr]!=i_ext[o_irq_addr])
-        irq_x[o_irq_addr] = 0;
-    
-    if(irq_x != 0 && i_en == 1) begin
-        for (j=30; j>=0; j=j-1 ) begin
-            if(irq_x[j])
-                o_irq_addr = j;
-        end        
-        o_IRQ = 1;
-        o_IRQ = 0;
-    end
+always@(negedge i_IRQ) begin //A interrupcao atual terminou o processamento
+    if(i_reg[4*o_irq_addr+2]!=0 || i_reg[4*o_irq_addr+1]!=0 || i_reg[4*o_irq_addr]!=i_ext[o_irq_addr]) //Se nao estiver ativa ao nivel
+        irq_x[o_irq_addr] = 0; //apaga a interrupcao individual a ser atendida
+    else
+        new_interrupt;
 end
 
-integer k;
+always@(irq_x) begin // Nova interrupcao
+    new_interrupt;
+end
 
-always@(irq_x) begin
-    if(i_IRQ == 0 && i_en == 1) begin
+task new_interrupt;
+    integer k;
+    if(irq_x != 0 && i_IRQ == 0 && i_en == 1) begin
         for (k=30; k>=0; k=k-1 ) begin
             if(irq_x[k])
                 o_irq_addr = k;
         end
         o_IRQ = 1;
+        #1
         o_IRQ = 0;
     end
-end
+endtask
  
 
     
