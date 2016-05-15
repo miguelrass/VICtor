4be3c0c31d8d6070bb583b017d22ba31d1b6645d
Miguel Rass <miguel_rass@hotmail.com>
2016-05-15 10:54:52 +0100

    nuevo

--- vic_irq.v
+++ vic_irq.v
@@ -27,29 +27,53 @@ module vic_irq(
     input i_rst,
     input [30:0] i_ext,
     input i_en,
-    output [4:0] o_irq_addr,
-    output reg  [30:0] irq_x //meter isto fora daqui
+    //output o_pending, //outra interrupcao alem da atual
+    output reg [4:0] o_irq_addr //num. interr atual
     );
     
-//reg  [30:0] irq_x;
+reg  [30:0] irq_x;
     
 genvar i;
 
 generate //verificar a necessidade de generate
     for (i=0; i<31; i=i+1 ) begin
         always@(i_ext[i]) begin 
-            irq_x[i] <= (irq_x[i] && i_reg[4*i+2])?                         1 : 
+            irq_x[i] = (irq_x[i] && i_reg[4*i+2])?                        1 : 
                        (~irq_x[i] && i_reg[4*i+1])?                        1 : 
                        (i_reg[4*i+2:4*i+1]==0 && i_reg[4*i+3]==irq_x[i])?  1 : 
-                                                                            irq_x[i];
+                                                                           irq_x[i];
         end 
     end
 endgenerate
 
-always@(negedge rst) begin 
+always@(rst) begin 
     irq_x <= 31'b0; 
     //ver mais registos pa fazer reset
 end 
+
+integer j;
+
+always@(negedge io_IRQ) begin
+    irq_x[o_irq_addr] = 0;
+    
+    if(irq_x != 0 && i_en == 1) begin
+        for (j=30; j>=0; j=j-1 ) begin
+            if(irq_x[j])
+                o_irq_addr = j;
+        end        
+        io_IRQ = 1;
+    end
+end
+
+always@(irq_x) begin
+    if(io_IRQ == 0 && i_en == 1) begin
+        for (j=30; j>=0; j=j-1 ) begin
+            if(irq_x[j])
+                o_irq_addr = j;
+        end
+        io_IRQ = 1;
+    end
+end
  
 
     
