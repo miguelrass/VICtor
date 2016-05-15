a1aff7e058f7920be3833f16cd9be34fa6d1f95a
Miguel Rass <miguel_rass@hotmail.com>
2016-05-15 09:35:06 +0100

    lol

--- vic_irq.v
+++ vic_irq.v
@@ -22,11 +22,36 @@
 
 module vic_irq(
     inout io_IRQ,
-    input [123:0] i_reg,
+    input [123:0] i_reg, //en/fall/rise/level
     input i_clk,
     input i_rst,
     input [30:0] i_ext,
     input i_en,
-    output [4:0] o_irq_addr
+    output [4:0] o_irq_addr,
+    output reg  [30:0] irq_x //meter isto fora daqui
     );
+    
+//reg  [30:0] irq_x;
+    
+genvar i;
+
+generate //verificar a necessidade de generate
+    for (i=0; i<31; i=i+1 ) begin
+        always@(i_ext[i]) begin 
+            irq_x[i] <= (irq_x[i] && i_reg[4*i+2])?                         1 : 
+                       (~irq_x[i] && i_reg[4*i+1])?                        1 : 
+                       (i_reg[4*i+2:4*i+1]==0 && i_reg[4*i+3]==irq_x[i])?  1 : 
+                                                                            irq_x[i];
+        end 
+    end
+endgenerate
+
+always@(negedge rst) begin 
+    irq_x <= 31'b0; 
+    //ver mais registos pa fazer reset
+end 
+ 
+
+    
+    
 endmodule
